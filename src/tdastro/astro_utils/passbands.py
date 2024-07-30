a216be3f8d2933d99b6b12d0b2ac9c99658cbb6f
Olivia Lynn <olynn@andrew.cmu.edu>
2024-07-30 09:56:09 -0400

    Add unit tests; Move plotting functions to notebook

--- src/tdastro/astro_utils/passbands.py
+++ src/tdastro/astro_utils/passbands.py
@@ -1,9 +1,9 @@
 import os
 import time
 import urllib.request
+from pathlib import Path
 from urllib.error import HTTPError, URLError
 
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy.integrate
 
@@ -18,59 +18,81 @@ class Passbands:
             self.bands = ["u", "g", "r", "i", "z", "y"]
         else:
             self.bands = bands
-        self.data_path = ""
+        self.data_path = None
         self.transmission_tables = {}
         self.normalized_system_response_tables = {}
 
     def _get_data_path(self):
-        """Find/create a data directory for the bandpass data, located next to passbands.py.
+        """Find/create a data directory for the bandpass data, located in same dir as passbands.py.
 
-        This is a temporary solution to be replaced when we implement Pooch throughout the project."""
-        script_path = os.path.realpath(__file__)
-        script_dir = os.path.dirname(script_path)
-        data_dir = os.path.join(script_dir, "band_data")
+        NOTE! This is a temporary solution to be replaced when we implement Pooch throughout the project."""
+        # script_path = os.path.realpath(__file__)
+        # script_dir = os.path.dirname(script_path)
+        # data_dir = os.path.join(script_dir, "band_data")
+
+        data_dir = os.path.join(Path(__file__).parent, "band_data")
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
         self.data_path = data_dir
 
-    def _load_single_transmission_table(self, band_name: str) -> bool:
-        """Load a transmission table from a file, downloading it first if necessary.
+    def _download_transmission_table(self, band_name: str, output_file: str, timeout: int = 30) -> bool:
+        """Download a transmission table for a single band and save it to a file.
 
         Files used are from: http://svo2.cab.inta-csic.es/svo/theory/fps3/index.php?mode=browse&gname=LSST
 
         Parameters
         ----------
         band_name : str
+            Name of the band for which the transmission table is to be downloaded.
+        output_file : str
+            Path to the file where the transmission table will be saved.
+        timeout : int
+            Time in seconds to wait for the file to be downloaded and created.
+
+        Returns
+        -------
+        bool
+            True if the transmission table is successfully downloaded, False otherwise.
+        """
+        try:
+            url = (
+                f"http://svo2.cab.inta-csic.es/svo/theory/fps3/getdata.php"
+                f"?format=ascii&id=LSST/LSST.{band_name}"
+            )
+            urllib.request.urlretrieve(url, output_file)
+            start_time = time.time()
+            while not os.path.exists(output_file):
+                if time.time() - start_time > timeout:
+                    print(f"Timeout reached while waiting for {output_file} to be created.")
+                    return False
+                time.sleep(1)
+            print(f"Downloaded: {band_name}-band transmission table.")
+            return True
+        except HTTPError as e:
+            print(f"HTTP error occurred when downloading transmission table for {band_name}: {e}")
+            return False
+        except URLError as e:
+            print(f"URL error occurred when downloading transmission table for {band_name}: {e}")
+            return False
+
+    def _load_transmission_table(self, band_name: str, file_path: str) -> bool:
+        """Load a transmission table from a file.
+
+        Parameters
+        ----------
+        band_name : str
             Name of the band for which the transmission table is to be loaded.
+        file_path : str
+            Path to the file containing the transmission table.
 
         Returns
         -------
         bool
             True if the transmission table is successfully loaded, False otherwise.
         """
-        file_path = f"{self.data_path}/LSST_LSST.{band_name}.dat"
-
-        # Download the file, if it doesn't exist
-        if not os.path.exists(file_path):
-            try:
-                url = (
-                    f"http://svo2.cab.inta-csic.es/svo/theory/fps3/getdata.php"
-                    f"?format=ascii&id=LSST/LSST.{band_name}"
-                )
-                urllib.request.urlretrieve(url, file_path)
-                while not os.path.exists(file_path):
-                    time.sleep(1)
-            except HTTPError as e:
-                print(f"HTTP error occurred when downloading transmission table for {band_name}: {e}")
-                return False
-            except URLError as e:
-                print(f"URL error occurred when downloading transmission table for {band_name}: {e}")
-                return False
-
-        # Load the file contents
         try:
             self.transmission_tables[band_name] = np.loadtxt(file_path)
-            print(f"{band_name}-band transmission table loaded.")
+            print(f"Loaded: {band_name}-band transmission table.")
             return True
         except ValueError as e:
             print(f"Value error when loading transmission table for {band_name}-band: {e}")
@@ -82,13 +104,34 @@ class Passbands:
     def load_all_transmission_tables(self) -> None:
         """Load all bandpass tables from a file."""
         self._get_data_path()
+
         for band_name in self.bands:
-            self._load_single_transmission_table(band_name)
+            file_path = f"{self.data_path}/LSST_LSST.{band_name}.dat"
+
+            # Download the file, if it doesn't exist
+            if not os.path.exists(file_path) and not self._download_transmission_table(band_name, file_path):
+                print(f"Transmission table for {band_name} could not be downloaded.")
+                continue
+
+            # Load the file contents
+            self._load_transmission_table(band_name, file_path)
 
     def _phi_b(self, transmission_table: np.ndarray) -> np.ndarray:
         """Calculate the value of phi_b for all wavelengths in a transmission table.
 
-        This is eq. 8 from the LSST Flux Units paper. (TODO write out the equation/proper citation)"""
+        This is eq. 8 from the LSST Flux Units paper. (TODO write out the equation/proper citation)
+
+        Parameters
+        ----------
+        transmission_table : np.ndarray
+            A 2D array with wavelengths (Angstroms) in the first column and transmission strengths in the
+            second.
+
+        Returns
+        -------
+        np.ndarray
+            An array of phi_b values for all wavelengths in the transmission table.
+        """
         # No interpolation in this version
         transmission_values = transmission_table[:, 1]
         wavelengths = transmission_table[:, 0]
@@ -103,7 +146,7 @@ class Passbands:
 
     def calculate_normalized_system_response_tables(self) -> None:
         """Calculate the normalized system response tables for all bands."""
-        for band_name in ["u", "g", "r", "i", "z", "y"]:
+        for band_name in self.bands:
             normalized_wavelengths_angstroms = self._phi_b(self.transmission_tables[band_name])
             self.normalized_system_response_tables[band_name] = np.column_stack(
                 (self.transmission_tables[band_name][:, 0], normalized_wavelengths_angstroms)
@@ -117,6 +160,19 @@ class Passbands:
         input wavelengths, but, this will be inconvenient for other types of models.
 
         TODO may want to make another version that interpolates the fluxes to the wavelength grid.
+
+        Parameters
+        ----------
+        flux : np.ndarray
+            Array of flux values evaluated at the wavelengths in the normalized system response table.
+        normalized_system_response_table : np.ndarray
+            A 2D array with wavelengths (Angstroms) in the first column and normalized system response values
+            in the second.
+
+        Returns
+        -------
+        float
+            The in-band flux value.
         """
         passband_wavelengths = normalized_system_response_table[:, 0]
         integrand = flux * normalized_system_response_table[:, 1]
@@ -125,15 +181,25 @@ class Passbands:
     def get_all_in_band_fluxes(self, model: PhysicalModel, times) -> np.ndarray:
         """Calculate the in-band fluxes for all bands.
 
-        NOTE model needs to be interpolated or somehow able to evaluate with arbitrary wavelengths.
+        NOTE given model needs to be interpolated or otherwise able to evaluate with arbitrary wavelengths.
+
+        Parameters
+        ----------
+        model : PhysicalModel
+            A model object that can evaluate fluxes at arbitrary wavelengths.
+        times : np.ndarray
+            Array of times at which to evaluate the model.
+
+        Returns
+        -------
+        np.ndarray
+            A 2D array with in-band fluxes for all bands at all times.
         """
-        bands = ["u", "g", "r", "i", "z", "y"]
-
         # Prepare an array to hold the flux values
-        flux_matrix = np.zeros((len(times), len(bands)))
+        flux_matrix = np.zeros((len(times), len(self.bands)))
 
-        # Vectorized computation
-        for i, band in enumerate(bands):
+        # Apply _get_in_band_flux to all fluxes
+        for i, band in enumerate(self.bands):
             wavelengths_in_band = self.normalized_system_response_tables[band][:, 0]
             # Evaluate the spline model for all times and wavelengths in the band
             # Note we'll be double-dipping here, in that some wavelengths do exist in multiple bands
@@ -145,48 +211,3 @@ class Passbands:
             )
             flux_matrix[:, i] = in_band_fluxes
         return flux_matrix
-
-    # Plotting functions
-
-    def plot_transmission_tables(self) -> None:
-        """Plot all transmission tables."""
-        plt.title("Transmission Tables")
-        plt.xlabel("Wavelength (Angstroms)")
-        plt.ylabel("Transmission")
-        plt.legend(self.bands)
-        for band_id in self.bands:
-            plt.plot(self.transmission_tables[band_id][:, 0], self.transmission_tables[band_id][:, 1])
-        plt.show()
-
-    def plot_single_transmission_table(self, band_name: str) -> None:
-        """Plot a single transmission table."""
-        plt.title(f"{band_name}-band Transmission Table")
-        plt.xlabel("Wavelength (Angstroms)")
-        plt.ylabel("Transmission")
-        plt.plot(self.transmission_tables[band_name][:, 0], self.transmission_tables[band_name][:, 1])
-        plt.show()
-
-    def plot_normalized_system_response_tables(self) -> None:
-        """Plot all normalized system response tables."""
-        plt.title("Normalized System Response Tables")
-        plt.xlabel("Wavelength (Angstroms)")
-        plt.ylabel("Normalized System Response (inverse Angstroms I guess? check this whole axis label)")
-        plt.legend(self.bands)
-        for band_id in self.bands:
-            plt.plot(
-                self.normalized_system_response_tables[band_id][:, 0],
-                self.normalized_system_response_tables[band_id][:, 1],
-            )
-        plt.show()
-
-    def plot_single_normalized_system_response_table(self, band_name: str) -> None:
-        """Plot a single normalized system response table."""
-        # TODO maybe reduce duplication with the plotting functions
-        plt.title(f"{band_name}-band Normalized System Response Table")
-        plt.xlabel("Wavelength (Angstroms)")
-        plt.ylabel("Normalized System Response (inverse Angstroms I guess? check this whole axis label)")
-        plt.plot(
-            self.normalized_system_response_tables[band_name][:, 0],
-            self.normalized_system_response_tables[band_name][:, 1],
-        )
-        plt.show()
---
+++ tests/tdastro/astro_utils/test_passbands.py
@@ -0,0 +1,219 @@
+import os
+import urllib
+from pathlib import Path
+from unittest.mock import patch
+
+import numpy as np
+from tdastro.astro_utils.passbands import Passbands
+from tdastro.sources.spline_model import SplineModel
+
+
+def test_passbands_init():
+    """Test that we can initialize a Passbands object with different bands."""
+    passbands_default = Passbands()
+    assert passbands_default.bands == ["u", "g", "r", "i", "z", "y"]
+
+    passbands_ubv = Passbands(bands=["u", "b", "v"])
+    assert passbands_ubv.bands == ["u", "b", "v"]
+
+
+def test_passbands_get_data_path(tmp_path):
+    """Test that we can find/create a data directory for the bandpass data."""
+    passbands = Passbands()
+
+    # Check that the data path is empty before we call the method
+    assert passbands.data_path is None
+
+    # Mock the os.path.join function to point to our tmp path
+    tmp_data_path = os.path.join(tmp_path, "band_data/")
+    with patch("os.path.join") as mocked_join:
+        mocked_join.return_value = tmp_data_path
+        passbands._get_data_path()
+
+        assert os.path.isdir(passbands.data_path)
+        assert os.path.isdir(Path(f"{tmp_path}/band_data/"))
+        assert passbands.data_path == Path(f"{tmp_path}/band_data/")
+
+
+def test_passbands_load_local_transmission_table():
+    """Test that we can load a transmission table from a file that already exists."""
+    expected_data = np.array([[100.0, 0.5], [200.0, 0.75], [300.0, 0.25]])
+    with patch("numpy.loadtxt", return_value=expected_data):
+        passbands = Passbands(bands="test-band")
+        passbands._load_transmission_table("test-band", "mock_file.dat")
+        np.testing.assert_array_equal(passbands.transmission_tables["test-band"], expected_data)
+
+
+def test_passbands_load_remote_transmission_table():
+    """Test that we can download a transmission table and load the file contents."""
+    with patch("urllib.request.urlretrieve", return_value=True):
+        with patch("os.path.exists", return_value=True):
+            passbands = Passbands(bands="test-band")
+            assert passbands._download_transmission_table("test-band", "mock_file.dat")
+
+
+def test_passbands_download_transmission_table_http_error():
+    """Test that we handle HTTPError when downloading a transmission table."""
+    passbands = Passbands(bands=["test-band"])
+    with patch(
+        "urllib.request.urlretrieve",
+        side_effect=urllib.error.HTTPError(url="", code=404, msg="Not Found", hdrs=None, fp=None),
+    ):
+        assert not passbands._download_transmission_table("test-band", "mock_file.dat")
+
+
+def test_passbands_download_transmission_table_url_error():
+    """Test that we handle URLError when downloading a transmission table."""
+    passbands = Passbands(bands=["test-band"])
+    with patch("urllib.request.urlretrieve", side_effect=urllib.error.URLError("No host")):
+        assert not passbands._download_transmission_table("test-band", "mock_file.dat")
+
+
+def test_passbands_load_transmission_table_value_error():
+    """Test that we handle ValueError when loading a transmission table."""
+    passbands = Passbands(bands=["test-band"])
+    with patch("numpy.loadtxt", side_effect=ValueError("Mocked ValueError")):
+        assert not passbands._load_transmission_table("test-band", "mock_file.dat")
+
+
+def test_passbands_load_transmission_table_os_error():
+    """Test that we handle OSError when loading a transmission table."""
+    passbands = Passbands(bands=["test-band"])
+    with patch("numpy.loadtxt", side_effect=OSError("Mocked OSError")):
+        assert not passbands._load_transmission_table("test-band", "mock_file.dat")
+
+
+def test_passbands_load_all_transmission_tables(tmp_path):
+    """Test that we can download and load all transmission tables."""
+    band_ids = ["u", "g"]
+    passbands = Passbands(bands=band_ids)
+
+    # Mock the _get_data_path method
+    with patch.object(passbands, "_get_data_path"):
+        passbands.data_path = os.path.join(tmp_path, "band_data/")
+
+        # Mock the _download_transmission_table and _load_transmission_table methods
+        with patch.object(passbands, "_download_transmission_table", return_value=True), patch.object(
+            passbands, "_load_transmission_table", return_value=True
+        ):
+            passbands.load_all_transmission_tables()
+
+            # Assert _download_transmission_table was called for each band
+            assert passbands._download_transmission_table.call_count == len(band_ids)
+
+            # Assert _load_transmission_table was called for each band
+            assert passbands._load_transmission_table.call_count == len(band_ids)
+
+
+def test_passbands_phi_b():
+    """Test that we can calculate the value of phi_b for all wavelengths in a transmission table."""
+    passbands = Passbands()
+
+    u_band_transmission_table = np.array([[100.0, 0.5], [200.0, 0.75], [300.0, 0.25]])
+    g_band_transmission_table = np.array([[100.0, 0.75], [200.0, 0.5], [300.0, 0.25]])
+
+    u_band_phi_b = passbands._phi_b(u_band_transmission_table)
+    g_band_phi_b = passbands._phi_b(g_band_transmission_table)
+
+    assert len(u_band_phi_b) == len(u_band_transmission_table)
+    assert len(g_band_phi_b) == len(g_band_transmission_table)
+
+    # Check that the integrated value of u_band_phi_b about-equals that of g_band_phi_b
+    assert np.isclose(
+        np.trapz(u_band_phi_b, x=u_band_transmission_table[:, 0]),
+        np.trapz(g_band_phi_b, x=g_band_transmission_table[:, 0]),
+        rtol=1e-9,  # relative tolerance
+        atol=1e-9,  # absolute tolerance
+    )
+
+    # And that they're both about equal to 1.0
+    assert np.isclose(np.trapz(u_band_phi_b, x=u_band_transmission_table[:, 0]), 1.0, rtol=1e-9, atol=1e-9)
+    assert np.isclose(np.trapz(g_band_phi_b, x=g_band_transmission_table[:, 0]), 1.0, rtol=1e-9, atol=1e-9)
+
+
+def test_passbands_calculate_normalized_system_response_tables():
+    """Test that we can calculate the normalized system response tables for all bands."""
+    band_ids = ["u", "g"]
+    passbands = Passbands(bands=band_ids)
+
+    # Add mock transmission tables
+    passbands.transmission_tables["u"] = np.array([[100.0, 0.5], [200.0, 0.75], [300.0, 0.25]])
+    passbands.transmission_tables["g"] = np.array([[100.0, 0.75], [200.0, 0.5], [300.0, 0.25]])
+
+    # Mock the _phi_b method
+    with patch.object(passbands, "_phi_b", return_value=np.array([0.5, 0.5, 0.5])):
+        passbands.calculate_normalized_system_response_tables()
+
+        # Check that the normalized system response tables have been calculated for each band
+        for band_id in band_ids:
+            assert band_id in passbands.normalized_system_response_tables  # band_id is in the dictionary
+            assert passbands.normalized_system_response_tables[band_id].shape == (3, 2)  # 3 rows, 2 columns
+            assert np.allclose(passbands.normalized_system_response_tables[band_id][:, 1], 0.5)  # vals = 0.5
+
+
+def test_passbands_get_in_band_flux():
+    """Test the calculation of in-band flux for given flux and normalized system response table."""
+    passbands = Passbands(bands=["test-band"])
+
+    # Mock transmission table data for test-band
+    normalized_system_response_table = np.array([[100.0, 0.5], [200.0, 0.75], [300.0, 0.25]])
+    flux = np.array([1.0, 2.0, 3.0])
+
+    expected_in_band_flux = np.trapz(
+        flux * normalized_system_response_table[:, 1], x=normalized_system_response_table[:, 0]
+    )
+
+    # Calculate in-band flux using the method
+    calculated_in_band_flux = passbands._get_in_band_flux(flux, normalized_system_response_table)
+
+    assert np.isclose(calculated_in_band_flux, expected_in_band_flux, rtol=1e-9, atol=1e-9)
+
+    # TODO maybe a different check - is this a place where we could try the color thing?
+
+
+def test_passbands_get_all_in_band_fluxes():
+    """Test that we can calculate the in-band fluxes for all bands given a PhysicalModel and times."""
+
+    class MockPhysicalModel:
+        """A mock PhysicalModel for testing purposes."""
+
+        def evaluate(self, times, wavelengths):
+            """Mock evaluate method returning a 2D array of fluxes."""
+            flux_matrix = np.zeros((len(times), len(wavelengths)))
+            for i, time in enumerate(times):
+                flux_matrix[i] = np.sin(wavelengths * time)  # Some mock flux values
+            return flux_matrix
+
+    passbands = Passbands(bands=["test-band"])
+
+    # Mock the normalized system response table for test-band
+    normalized_system_response_table = np.array([[100.0, 0.5], [200.0, 0.75], [300.0, 0.25]])
+    passbands.normalized_system_response_tables["test-band"] = normalized_system_response_table
+
+    # Define some mock times
+    times = np.array([0.1, 0.2, 0.3])
+
+    # Create a mock PhysicalModel instance
+    # model = MockPhysicalModel()
+    model = SplineModel(
+        times,
+        [100.0, 200.0, 300.0],
+        np.array([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]),
+        time_degree=1,
+        wave_degree=1,
+    )
+
+    # Calculate expected flux matrix
+    expected_flux_matrix = np.zeros((len(times), len(passbands.bands)))
+    for i, time in enumerate(times):
+        # flux = np.sin(normalized_system_response_table[:, 0] * time)
+        flux = model.evaluate(np.array([time]), normalized_system_response_table[:, 0])
+        expected_flux = np.trapz(
+            flux * normalized_system_response_table[:, 1], x=normalized_system_response_table[:, 0]
+        )
+        expected_flux_matrix[i, 0] = expected_flux[0]
+
+    # Calculate in-band fluxes using the method
+    calculated_flux_matrix = passbands.get_all_in_band_fluxes(model, times)
+
+    np.testing.assert_allclose(calculated_flux_matrix, expected_flux_matrix, rtol=1e-9, atol=1e-9)
