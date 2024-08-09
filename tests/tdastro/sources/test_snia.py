c7986321525547cc38cd4074a4e1585c6e529608
Mi Dai <mdai23443@gmail.com>
2024-08-09 14:13:23 -0400

    add plots for sed and light curve

--- tests/tdastro/sources/test_snia.py
+++ tests/tdastro/sources/test_snia.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tdastro.astro_utils.passbands import Passbands
 from tdastro.astro_utils.snia_utils import DistModFromRedshift, HostmassX1Func, X0FromDistMod
 from tdastro.effects.redshift import Redshift
 from tdastro.sources.sncomso_models import SncosmoWrapperModel
@@ -13,7 +14,7 @@ def test_snia():
         ra=NumpyRandomFunc("uniform", low=0.0, high=360.0),
         dec=NumpyRandomFunc("uniform", low=-90.0, high=90.0),
         hostmass=NumpyRandomFunc("uniform", low=7, high=12),
-        redshift=NumpyRandomFunc("uniform", low=0.01, high=0.5),
+        redshift=NumpyRandomFunc("uniform", low=0.01, high=1.0),
     )
 
     distmod_func = DistModFromRedshift(host.redshift, H0=73.0, Omega_m=0.3)
@@ -31,7 +32,7 @@ def test_snia():
     )
 
     source = SncosmoWrapperModel(
-        "salt3",
+        "salt2-h17",
         t0=0,
         x0=x0_func,
         x1=x1_func,
@@ -43,20 +44,28 @@ def test_snia():
 
     source.add_effect(Redshift(redshift=source.redshift, t0=source.t0))
 
-    phase = np.linspace(-20, 30, 25)
+    phase = np.linspace(-5, 30, 20)
     # times = phase
     wavelengths = np.linspace(3000, 8000, 200)
     # print(phase)
     # print(wavelengths)
     # print("parameters (x0,x1,c,hostmass,ra,dec,redshift):")
 
-    res = {"wavelengths": wavelengths, "phase": phase, "flux": [], "parameter_values": []}
+    res = {"wavelengths": wavelengths, "phase": phase, "flux": [], "parameter_values": [], "bandfluxes": []}
 
-    for _n in range(0, 100):
+    passbands = Passbands(["g", "r", "i", "z"])
+
+    passbands.load_all_transmission_tables()
+    passbands.calculate_normalized_system_response_tables()
+
+    for _n in range(0, 10):
         flux = source.evaluate(phase, wavelengths, resample_parameters=True)
         p = source.get_all_parameter_values(True)
 
         res["parameter_values"].append(p)
         res["flux"].append(flux)
+
+        bandfluxes = passbands.get_all_in_band_fluxes(source, phase)
+        res["bandfluxes"].append(bandfluxes)
 
     return res
