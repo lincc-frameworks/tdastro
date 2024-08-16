e34731e51b6a5447d81c6da9617cdc8d8a09f3f3
Olivia Lynn <olynn@andrew.cmu.edu>
2024-08-15 23:52:55 -0400

    Improve docstrings

--- src/tdastro/astro_utils/passbands.py
+++ src/tdastro/astro_utils/passbands.py
@@ -30,20 +30,27 @@ class PassbandGroup:
             A list of Passband objects assigned to the group.
         """
         if preset is not None:
-            self.load_preset(preset)
+            self._load_preset(preset)
 
         if passbands is not None:
             for passband in passbands:
-                self.passbands[passband.label] = passband  # Overriding any preset passbands
+                self.passbands[passband.label] = passband  # This overrides any preset passbands
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return a string representation of the PassbandGroup."""
         return (
             f"PassbandGroup containing {len(self.passbands)} passbands: "
             f"{', '.join([passband.full_name for passband in self.passbands.values()])}"
         )
 
-    def load_preset(self, preset):
-        """Load a pre-defined set of passbands."""
+    def _load_preset(self, preset):
+        """Load a pre-defined set of passbands.
+
+        Parameters
+        ----------
+        preset : str
+            The name of the pre-defined set of passbands to load.
+        """
         if preset == "LSST":
             self.passbands = {
                 "u": Passband("LSST", "u"),
@@ -57,7 +64,20 @@ class PassbandGroup:
             raise ValueError(f"Unknown passband preset: {preset}")
 
     def fluxes_to_bandfluxes(self, flux_density_matrix, wavelengths_angstrom) -> np.ndarray:
-        """Calculate bandfluxes for all passbands in the group."""
+        """Calculate bandfluxes for all passbands in the group.
+
+        Parameters
+        ----------
+        flux_density_matrix : np.ndarray
+            A 2D array of flux densities where rows are times and columns are wavelengths.
+        wavelengths_angstrom : np.ndarray
+            An array of wavelengths in Angstroms corresponding to the columns of flux_density_matrix.
+
+        Returns
+        -------
+        dict
+            A dictionary of bandfluxes with passband labels as keys.
+        """
         bandfluxes = {}
         for label, passband in self.passbands.items():
             bandfluxes[label] = passband.fluxes_to_bandflux(flux_density_matrix, wavelengths_angstrom)
@@ -77,7 +97,14 @@ class Passband:
         self.normalized_transmission_table = self._normalize_transmission_table()
 
     def _load_transmission_table(self) -> np.ndarray:
-        """Load a transmission table from a file or URL."""
+        """Load a transmission table from a file or URL.
+
+        Returns
+        -------
+        np.ndarray
+            A 2D array where the first column is wavelengths (Angstrom) and the second column is transmission
+            values.
+        """
         if self.table_path is None:
             self.table_path = os.path.join(
                 os.path.dirname(__file__), f"passbands/{self.survey}/{self.label}.dat"
@@ -87,8 +114,14 @@ class Passband:
             self._download_transmission_table()
         return np.loadtxt(self.table_path)
 
-    def _download_transmission_table(self) -> None:
-        """Download a transmission table from a URL."""
+    def _download_transmission_table(self) -> bool:
+        """Download a transmission table from a URL.
+
+        Returns
+        -------
+        bool
+            True if the download was successful, False otherwise.
+        """
         if self.table_url == "":
             if self.survey == "LSST":
                 # TODO consider: https://github.com/lsst/throughputs/blob/main/baseline/filter_g.dat
@@ -146,11 +179,17 @@ class Passband:
         return np.column_stack((wavelengths_angstrom, normalized_transmissions))
 
     def fluxes_to_bandflux(self, flux_density_matrix, wavelengths_angstrom) -> np.ndarray:
-        """Calculate the bandflux for a given set of flux densities."""
+        """Calculate the bandflux for a given set of flux densities.
+
+        This is eq. 7 from "On the Choice of LSST Flux Units" (Ivezić et al.):
+
+        F_b = ∫ f(λ)φ_b(λ) dλ
+
+        where f(λ) is the specific flux of an object at the top of the atmosphere, and φ_b(λ) is the
+        normalized system response for given band b."
+        """
         # Check that the grid of wavelengths_angstrom matches the band's transmission table
-        # If needed, this is where we would handle interpolation
-        # Note that np.searchsorted will automatically handle cases where the flux density grid is coarser
-        # than the transmission table grid (though not with interpolation!)
+        # NOTE: this is where we would handle interpolation and exterpolation as needed--PR incoming!
         # For now, min check: the bounds of the flux density grid are within the transmission table bounds
         if (
             wavelengths_angstrom[-1] < self.normalized_transmission_table[-1, 0]
@@ -162,7 +201,7 @@ class Passband:
                 f"[{self.normalized_transmission_table[0, 0]}, {self.normalized_transmission_table[-1, 0]}]."
             )
 
-        # Get only the flux densities that are within the passband
+        # Get only the flux densities that are within the passband's wavelength range
         passband_wavelengths = self.normalized_transmission_table[:, 0]
         passband_wavelengths_indices = np.searchsorted(wavelengths_angstrom, passband_wavelengths)
         passband_flux_density_matrix = flux_density_matrix[:, passband_wavelengths_indices]
