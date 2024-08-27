7a99d967eb0a34ddf2a60c7889405dd9b472fc33
mi-dai <mdai23443@gmail.com>
2024-08-27 16:14:41 -0400

    Update src/tdastro/astro_utils/unit_utils.py
    Co-authored-by: Konstantin Malanchev <hombit@gmail.com>

--- src/tdastro/astro_utils/unit_utils.py
+++ src/tdastro/astro_utils/unit_utils.py
@@ -27,6 +27,6 @@ def flam_to_fnu(flux_flam, wavelengths, wave_unit=None, flam_unit=None, fnu_unit
     flux_flam = flux_flam * flam_unit
 
     # convert flux in flam_unit (e.g. ergs/s/cm^2/A) to fnu_unit (e.g. nJy or ergs/s/cm^2/Hz)
-    flux_fnu = (flux_flam * (wavelengths * wave_unit) ** 2 / const.c).to(fnu_unit).value
+    flux_fnu = (flux_flam * (wavelengths * wave_unit) ** 2 / const.c).to_value(fnu_unit)
 
     return flux_fnu
