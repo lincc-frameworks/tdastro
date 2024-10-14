52c0aa662a12d458967548fa0715ad27aa71a4be
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2024-10-14 10:48:05 -0400

    Update src/tdastro/astro_utils/snia_utils.py
    Co-authored-by: Konstantin Malanchev <hombit@gmail.com>

--- src/tdastro/astro_utils/snia_utils.py
+++ src/tdastro/astro_utils/snia_utils.py
@@ -235,7 +235,7 @@ class HostmassX1Func(NumericalInversePolynomialFunc):
         graph_state : `GraphState`
             An object mapping graph parameters to their values. This object is modified
             in place as it is sampled.
-        rng_info : numpy.random._generator.Generator, optional
+        rng_info : numpy.random._generator.Generator or None, optional
             A given numpy random number generator to use for this computation. If not
             provided, the function uses the node's random number generator.
         **kwargs : `dict`, optional
