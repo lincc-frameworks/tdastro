70da9c19eea9fe4eef3760624dbc8e4bac5d1f29
Mi Dai <mdai23443@gmail.com>
2024-10-25 14:11:08 -0400

    add phot and spec efficiency functions

--- src/tdastro/astro_utils/obs_utils.py
+++ src/tdastro/astro_utils/obs_utils.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 def phot_eff_function(snr):
-
     """
     Photometric detection efficiency as a simple step function of snr.
 
@@ -18,13 +17,12 @@ def phot_eff_function(snr):
     """
 
     snr = np.array(snr)
-    eff = np.where(snr > 5, 1., 0.)
+    eff = np.where(snr > 5, 1.0, 0.0)
 
     return eff
 
 
 def spec_eff_function(peak_imag):
-
     """
     Spectroscopic follow-up efficiency as a function of peak i band magnitude.
 
@@ -38,31 +36,14 @@ def spec_eff_function(peak_imag):
     eff: `list` or `numpy.ndarray`
         The spectroscopic efficiency given peak i band magnitude.
         Based on Equation (17) in Kessler et al. 2019
+        s0, s1, s2 are fitted using data from Figure 4 in Kessler et al. 2019
     """
 
-    s0 =
-    s1 = 
-    s2 = 
+    s0 = 1.0
+    s1 = 2.36
+    s2 = 51.9
 
     peak_imag = np.array(peak_imag)
-    eff = s0 * np.power((1.+ np.exp(s1 * peak_imag - s2)), -1)
+    eff = s0 * np.power((1.0 + np.exp(s1 * peak_imag - s2)), -1)
 
     return eff
-
-
-
-class Detection_Efficiency(FunctionNode):
-
-    def __init__():
-
-
-
-
-
-class Spectroscopic_Efficiency(FunctionNode):
-
-    def __init__():
-
-
-
-
