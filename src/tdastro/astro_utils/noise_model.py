a3d54c92eff3e89a575775de67b63789ff099495
Konstantin Malanchev <hombit@gmail.com>
2025-02-28 14:35:57 -0500

    Change argument units for the noise model

--- src/tdastro/astro_utils/noise_model.py
+++ src/tdastro/astro_utils/noise_model.py
@@ -5,7 +5,6 @@ import numpy.typing as npt
 def poisson_bandflux_std(
     bandflux: npt.ArrayLike,
     *,
-    pixel_scale: npt.ArrayLike,
     total_exposure_time: npt.ArrayLike,
     exposure_count: npt.ArrayLike,
     footprint: npt.ArrayLike,
@@ -20,9 +19,6 @@ def poisson_bandflux_std(
     ----------
     bandflux : array_like of float
         Source bandflux in energy units, e.g. nJy.
-    pixel_scale : array_like of float
-        Pixel scale of the detector,
-        in angular units (e.g. arcsec) per pixel.
     total_exposure_time : array_like of float
         Total exposure time of all observation, in time units
         (e.g. seconds).
@@ -30,14 +26,14 @@ def poisson_bandflux_std(
         Number of exposures in the observation.
     sky : array_like of float
         Sky background per unit angular area,
-        in the units of flux / pixel_scale * pixel, e.g.
-        nJy / arcsec^2.
+        in the units of electrons / pixel^2.
     footprint : array_like of float
-        Point spread function effective area,
-        in squared angular units, e.g. arcsec^2.
+        Point spread function effective area, in pixel^2.
     zp : array_like of float
         Zero point bandflux for the observation, i.e. bandflux
         giving a single electron during the total exposure time.
+        Units are the same as the input bandflux over electron,
+        e.g. nJy / electron.
     readout_noise : array_like of float
         Standard deviation of the readout electrons per pixel per exposure.
     dark_current : array_like of float
@@ -65,13 +61,11 @@ def poisson_bandflux_std(
     The output is Poisson standard deviation of the sum of all
     these noises converted to the flux units.
     """
-    area_px = footprint / pixel_scale**2
-
     # Get variances, in electrons^2
     source_variance = bandflux / zp
-    sky_variance = sky * footprint / zp
-    readout_variance = readout_noise**2 * area_px * exposure_count
-    dark_variance = dark_current * total_exposure_time * area_px
+    sky_variance = sky * footprint
+    readout_variance = readout_noise**2 * footprint * exposure_count
+    dark_variance = dark_current * total_exposure_time * footprint
 
     total_variance = source_variance + sky_variance + readout_variance + dark_variance
 
