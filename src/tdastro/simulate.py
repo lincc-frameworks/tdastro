e2fccf6a9b37ffff4ac58125c3c95e91a92d6e1f
Mi Dai <mdai23443@gmail.com>
2025-03-25 10:36:39 -0400

    address comments

--- src/tdastro/simulate.py
+++ src/tdastro/simulate.py
@@ -91,7 +91,7 @@ def simulate_lightcurves(source, num_samples, opsim, passbands, rng=None):
         results_dict["ra"].append(ra[idx])
         results_dict["dec"].append(dec[idx])
         results_dict["nobs"].append(len(obs_times))
-        results_dict["z"].append(state["host.redshift"])
+        results_dict["z"].append(source.get_param(state, "redshift"))
         results_dict["params"].append(state.to_dict())
 
         # Append the per-observation data to the nested dictionary.
