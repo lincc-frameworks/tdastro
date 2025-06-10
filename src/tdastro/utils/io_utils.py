a0881098c28f9ce822441fc43971e1c2967abde4
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-06-10 10:11:06 -0400

    Create a simple reader for snana lightcurve files

--- src/tdastro/utils/io_utils.py
+++ src/tdastro/utils/io_utils.py
@@ -1,3 +1,4 @@
+import gzip
 import logging
 from pathlib import Path
 
@@ -85,3 +86,132 @@ def read_grid_data(input_file, format="ascii", validate=False):
     values = data[v_col].data.reshape((len(x0), len(x1)))
 
     return x0, x1, values
+
+
+def _read_snana_lc_from_text_file(file_ptr):
+    """Read a SNANA outut lightcurve from a file.
+
+    Parameters
+    ----------
+    file_ptr : file object
+        The file pointer to read the SNANA light curve from.
+
+    Returns
+    -------
+    lc : astropy.table.Table
+        An Astropy Table containing the light curve data.
+    spec_tables : list
+        A list of spectra tables (if present in the file).
+    """
+    line_num = 0
+
+    # Data for the observation table.
+    meta = {}
+    table_data = {}
+    col_names = []
+
+    # Data for any spectrum tables.
+    spec_col_names = []
+    spec_tables = []
+    current_spec_data = {}
+    current_spec_meta = {}
+
+    for line in file_ptr:
+        # Strip out the trailing comment. Then skip lines that are either
+        # empty or do not contain a key-value pair.
+        line = line.split("#")[0].strip()
+        if not line or ":" not in line:
+            continue
+
+        # Split the line into key and value.
+        key, value = line.split(":", 1)
+
+        # The SNANA output file consists of a few blocks: general metadata,
+        # observations in a table, and possibly multiple spectrum tables.
+        # We determine which block we are in by the key.
+        if key == "VARLIST":
+            # If we are reading the observation table header, initialize the table_data
+            col_names = value.split()
+            for var_name in col_names:
+                table_data[var_name] = []
+        elif key == "OBS":
+            # If we are reading the obsertvation table data, split the line into
+            # values for each variable.
+            values = value.split()
+            if len(values) != len(col_names):
+                raise ValueError(
+                    f"Data error at line {line_num}: Expected {len(col_names)} values "
+                    f" but found {len(values)}."
+                )
+            for var_name, val in zip(col_names, values, strict=False):
+                table_data[var_name].append(val)
+        elif key == "VARNAMES_SPEC":
+            # If we are reading a spectrum table header, initialize the spec_col_names
+            spec_col_names = value.split()
+        elif key == "SPECTRUM_ID":
+            # If we are starting a new spectrum table, save the current one
+            # and reset the current_spec_data.
+            if len(current_spec_data) > 0:
+                spec_tables.append(Table(current_spec_data, meta=current_spec_meta))
+            current_spec_data = {key: [] for key in spec_col_names}
+            current_spec_meta = {"SPECTRUM_ID": value.strip()}
+        elif key.startswith("SPECTRUM_"):
+            # Save the spectrum metadata.
+            current_spec_meta[key] = value.strip()
+        elif key == "SPEC":
+            # Save the spectrum data lines.
+            values = value.split()
+            if len(values) != len(spec_col_names):
+                raise ValueError(
+                    f"Data error at line {line_num}: Expected {len(spec_col_names)} values "
+                    f" but found {len(values)}."
+                )
+            for var_name, val in zip(spec_col_names, values, strict=False):
+                current_spec_data[var_name].append(val)
+        else:
+            # Save everything else to the meta dictionary.
+            meta[key] = value.strip()
+
+        line_num += 1
+
+    # Put the observation data and the last spectrum data into tables.
+    lc = Table(table_data, meta=meta)
+    if len(current_spec_data) > 0:
+        spec_tables.append(Table(current_spec_data, meta=current_spec_meta))
+
+    return lc, spec_tables
+
+
+def read_snana_lc(file_path):
+    """Read a SNANA output light curve from a file.
+
+    Parameters
+    ----------
+    file_path : str or Path
+        The path to the SNANA light curve file.
+
+    Returns
+    -------
+    lc : astropy.table.Table
+        An Astropy Table containing the light curve data.
+    spec_tables : list
+        A list of spectra tables (if present in the file).
+    """
+    logging.debug(f"Loading SNANA light curve from {file_path}")
+    input_file = Path(file_path)
+    if not input_file.is_file():
+        raise FileNotFoundError(f"File {file_path} not found.")
+
+    if ".fits" in file_path.suffixes:
+        lc = None
+        raise NotImplementedError("Reading SNANA light curves from FITS files is not implemented yet.")
+    elif file_path.suffix == ".gz":
+        # Open as a gzipped text file.
+        with gzip.open(input_file, "rt") as file_ptr:
+            lc, spec_tables = _read_snana_lc_from_text_file(file_ptr)
+    else:
+        # Try to open the file as a regular text file.
+        with open(input_file, "r") as file_ptr:
+            lc, spec_tables = _read_snana_lc_from_text_file(file_ptr)
+
+    return lc, spec_tables
--- tests/tdastro/utils/test_io_utils.py
+++ tests/tdastro/utils/test_io_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from tdastro.utils.io_utils import read_grid_data
+from tdastro.utils.io_utils import read_grid_data, read_snana_lc
 
 
 def test_read_grid_data_good(grid_data_good_file):
@@ -27,3 +27,28 @@ def test_read_grid_data_bad(grid_data_bad_file):
     # We fail when loading a nonexistent file.
     with pytest.raises(FileNotFoundError):
         _, _, _ = read_grid_data("no_such_file_here", format="ascii", validate=True)
+
+
+def test_read_snana_lc(test_data_dir):
+    """Test reading a SNANA light curve from a text file."""
+    lc_file = test_data_dir / "test_snana_lc.dat"
+    lc, spec = read_snana_lc(lc_file)
+    assert len(lc) == 10
+    assert len(spec) == 2
+    assert len(spec[0]) == 5
+    assert len(spec[1]) == 8
+
+    # Check that the lightcurve has the expected columns.
+    assert "MJD" in lc.colnames
+    assert "FLT" in lc.colnames
+    assert "FIELD" in lc.colnames
+    assert "FLUXCAL" in lc.colnames
+    assert "FLUXCALERR" in lc.colnames
+    assert "MAG" in lc.colnames
+    assert "MAGERR" in lc.colnames
+
+    # Check that the specrum tables have the expected columns.
+    expected_spec_cols = ["LAMMIN", "LAMMAX", "FLAM", "FLAMERR", "SPECFLAG"]
+    for spec_table in spec:
+        for col_name in expected_spec_cols:
+            assert col_name in spec_table.colnames
