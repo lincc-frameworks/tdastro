salt2jax model compute_flux was taking phase instead of times and was giving wrong values. updated to calculate phase from times, and updated tests to use non-zero t0

tests/tdastro/sources/test_salt2.py
@@ -11,17 +11,17 @@ def test_salt2_model_parity(test_data_dir):
     results as the sncosmo version.
     """
     dir_name = test_data_dir / "truncated-salt2-h17"
-    td_model = SALT2JaxModel(x0=0.4, x1=0.3, c=1.1, t0=0.0, model_dir=dir_name)
+    td_model = SALT2JaxModel(x0=0.4, x1=0.3, c=1.1, t0=10.0, model_dir=dir_name)
 
     # We need to overwrite the source parameter to correspond to
     # the truncated directory data.
-    sn_model = SncosmoWrapperModel("SALT2", x0=0.4, x1=0.3, c=1.1, t0=0.0)
+    sn_model = SncosmoWrapperModel("SALT2", x0=0.4, x1=0.3, c=1.1, t0=10.0)
     sn_model.source = SALT2Source(modeldir=dir_name)
 
     # Test compared to values computed via sncosmo's implementation that
     # fall within the range of the truncated grid. We multiple by 1e12
     # for comparison precision purposes.
-    times = np.arange(-1.0, 15.0, 0.01)
+    times = np.arange(-1.0, 15.0, 0.01) + 10.0
     waves = np.arange(3800.0, 4200.0, 0.5)
 
     # Allow TDAstro to return both sets of results in f_nu.