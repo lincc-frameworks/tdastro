4e985f17d9922d1f4d964446ea0570aa4b782842
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-08-29 10:20:16 -0400

    New base class for survey data

---
+++ src/tdastro/opsim/survey_data.py
@@ -0,0 +1,446 @@
+"""The top-level module for survey related data, such as pointing and noise
+information. The SurveyData class is an abstract base class with specific implementations
+for different survey data, such as Rubin and ZTF."""
+
+import sqlite3
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from scipy.spatial import KDTree
+
+
+class SurveyData:
+    """A wrapper class around the survey table with helper computation functions and
+    cached data for efficiency.
+
+    Parameters
+    ----------
+    table : dict or pandas.core.frame.DataFrame
+        The table with all the survey information.
+    colmap : dict, optional
+        A mapping of standard column names to their names in the input table.
+        For example, in Rubin's OpSim we might have the column "observationStartMJD"
+        which maps to "time". In that case we would have an entry with key="time"
+        and value="observationStartMJD".
+    **kwargs : dict
+        Additional keyword arguments to pass to the constructor. This can include
+        overrides of any of the survey values.
+
+    Attributes
+    ----------
+    table : pandas.core.frame.DataFrame
+        The table with all the survey information mapped to standard column names.
+    survey_values : dict, optional
+        A mapping for constant values for the survey used in various computations, such
+        as readout noise and dark current.
+    _kd_tree : scipy.spatial.KDTree or None
+        A kd_tree of the survey pointings for fast spatial queries. We use the scipy
+        kd-tree instead of astropy's functions so we can directly control caching.
+    """
+
+    _required_columns = ["ra", "dec", "time"]
+
+    # Default survey values. These are all None for the abstract base class.
+    _default_survey_values = {
+        "dark_current": None,
+        "ext_coeff": None,
+        "pixel_scale": None,
+        "radius": None,
+        "read_noise": None,
+        "zp_per_sec": None,
+    }
+
+    # Class constants for the column names.
+    def __init__(
+        self,
+        table,
+        *,
+        colmap=None,
+        **kwargs,
+    ):
+        # Create a copy of the table.
+        if isinstance(table, dict):
+            self.table = pd.DataFrame(table)
+        else:
+            self.table = table.copy()
+
+        # Remap the columns to standard names. Start with the existing names (from the table)
+        # and overwrite anything provided by the column map.
+        name_map = {col: col for col in self.table.columns}
+        if colmap is not None:
+            for key, value in colmap.items():
+                if value in name_map:
+                    # Check for collisions (mapping a column to an existing column)
+                    if key in self.table.columns and key != value:
+                        raise ValueError(f"Trying to map {value} to {key}, but {key} is already a column.")
+                    name_map[value] = key
+        self.table.rename(columns=name_map, inplace=True)
+
+        # Check that we have the required columns.
+        for col in self._required_columns:
+            if col not in self.table.columns:
+                raise KeyError(f"Missing required column: {col}")
+
+        # If we are not given zero point data, try to derive it from the other columns.
+        if "zp" not in self.table.columns:
+            self._assign_zero_points()
+
+        # Save the survey values, overwriting anything that is manually specified
+        # as a keyword argument.
+        self.survey_values = self._default_survey_values.copy()
+        for key, value in kwargs.items():
+            self.survey_values[key] = value
+
+        # Build the kd-tree.
+        self._kd_tree = None
+        self._build_kd_tree()
+
+    def __len__(self):
+        return len(self.table)
+
+    def __getitem__(self, key):
+        """Access the underlying survey table by column name."""
+        return self.table[key]
+
+    @property
+    def columns(self):
+        """Get the column names."""
+        return self.table.columns
+
+    @classmethod
+    def from_db(cls, filename, sql_query="SELECT * FROM observations", **kwargs):
+        """Create an SurveyData object from the data in an db file. Reads data matching
+        what is produced by write_db (and matching the RubinOpsim table).
+
+        Parameters
+        ----------
+        filename : str
+            The name of the db file.
+        sql_query : str
+            The SQL query to use when loading the table.
+            Default: "SELECT * FROM observations"
+        kwargs : dict, optional
+            Additional keyword arguments to pass to the SurveyData constructor.
+
+        Returns
+        -------
+        SurveyData
+            A table with all of the pointing data.
+
+        Raise
+        -----
+        FileNotFoundError if the file does not exist.
+        ValueError if unable to load the table.
+        """
+        if not Path(filename).is_file():
+            raise FileNotFoundError(f"db file {filename} not found.")
+        con = sqlite3.connect(f"file:{filename}?mode=ro", uri=True)
+
+        # Read the table.
+        try:
+            survey_data = pd.read_sql_query(sql_query, con)
+        except Exception:
+            raise ValueError("Database read failed.") from None
+
+        # Close the connection.
+        con.close()
+
+        return SurveyData(survey_data, **kwargs)
+
+    def get_filters(self):
+        """Get the unique filters in the SurveyData table."""
+        if "filter" not in self.table.columns:
+            raise KeyError("No filters column found in SurveyData table.")
+        return np.unique(self.table["filter"])
+
+    def _build_kd_tree(self):
+        """Construct the KD-tree from the SurveyData table."""
+        ra_rad = np.radians(self.table["ra"].to_numpy())
+        dec_rad = np.radians(self.table["dec"].to_numpy())
+        # Convert the pointings to Cartesian coordinates on a unit sphere.
+        x = np.cos(dec_rad) * np.cos(ra_rad)
+        y = np.cos(dec_rad) * np.sin(ra_rad)
+        z = np.sin(dec_rad)
+        cart_coords = np.array([x, y, z]).T
+
+        # Construct the kd-tree.
+        self._kd_tree = KDTree(cart_coords)
+
+    def _assign_zero_points(self):
+        """Assign instrumental zero points in nJy to the data table.
+
+        Default implementation does not produce a zeropoint column. Subclasses
+        should override this method with a survey specific computation.
+        """
+        pass
+
+    def add_column(self, colname, values, *, overwrite=False):
+        """Add a column to the current survey data table.
+
+        Parameters
+        ----------
+        colname : str
+            The name of the new column.
+        values : int, float, str, list, or numpy.ndarray
+            The value(s) to add.
+        overwrite : bool
+            Overwrite the column is it already exists.
+            Default: False
+        """
+        if colname in self.table.columns and not overwrite:
+            raise KeyError(f"Column {colname} already exists.")
+
+        # If the input is a scalar, turn it into an array of the correct length
+        if np.isscalar(values):
+            values = np.full((len(self.table)), values)
+        self.table[colname] = values
+
+    def write_db(self, filename, *, tablename="observations", overwrite=False):
+        """Write out a survey data database to a given SQL table.
+
+        Parameters
+        ----------
+        filename : str
+            The name of the db file.
+        tablename : str
+            The table to which to write.
+            Default: "observations"
+        overwrite : bool
+            Overwrite the existing DB file.
+            Default: False
+
+        Raise
+        -----
+        FileExistsError if the file already exists and overwrite is False.
+        """
+        if_exists = "replace" if overwrite else "fail"
+
+        con = sqlite3.connect(filename)
+        try:
+            self.table.to_sql(tablename, con, if_exists=if_exists)
+        except Exception:
+            raise ValueError("Database write failed.") from None
+
+        con.close()
+
+    def time_bounds(self):
+        """Returns the min and max times for all observations in the SurveyData.
+
+        Returns
+        -------
+        t_min, t_max : float, float
+            The min and max times for all observations in the SurveyData.
+        """
+        t_min = self.table["time"].min()
+        t_max = self.table["time"].max()
+        return t_min, t_max
+
+    def filter_rows(self, rows):
+        """Filter the rows in the SurveyData to only include those indices that are provided
+        in a list of row indices (integers) or marked True in a mask.
+
+        Parameters
+        ----------
+        rows : numpy.ndarray
+            Either a Boolean array of the same length as the table or list of integer
+            row indices to keep.
+
+        Returns
+        -------
+        new_survey_data : SurveyData
+            A new SurveyData object with the reduced rows.
+        """
+        # Check if we are dealing with a mask of a list of indices.
+        rows = np.asarray(rows)
+        if rows.dtype == bool:
+            if len(rows) != len(self.table):
+                raise ValueError(
+                    f"Mask length mismatch. Expected {len(self.table)} rows, but found {len(rows)}."
+                )
+            mask = rows
+        else:
+            mask = np.full((len(self.table),), False)
+            mask[rows] = True
+
+        # Do the actual filtering and generate a new SurveyData. This automatically creates
+        # the cached data, such as the KD-tree. Note that we do not need to pass in all
+        # the defaults and keyword arguments, because the table is already fully formed.
+        new_survey_data = SurveyData(self.table[mask])
+        return new_survey_data
+
+    def is_observed(self, query_ra, query_dec, radius=None, t_min=None, t_max=None):
+        """Check if the query point(s) fall within the field of view of any
+        pointing in the survey.
+
+        Parameters
+        ----------
+        query_ra : float or numpy.ndarray
+            The query right ascension (in degrees).
+        query_dec : float or numpy.ndarray
+            The query declination (in degrees).
+        radius : float or None, optional
+            The angular radius of the observation (in degrees).
+        t_min : float or None, optional
+            The minimum time (in MJD) for the observations to consider.
+            If None, no time filtering is applied.
+        t_max : float or None, optional
+            The maximum time (in MJD) for the observations to consider.
+            If None, no time filtering is applied.
+
+        Returns
+        -------
+        seen : bool or list[bool]
+            Depending on the input, this is either a single bool to indicate
+            whether the query point is observed or a list of bools for an array
+            of query points.
+        """
+        inds = self.range_search(query_ra, query_dec, radius, t_min=t_min, t_max=t_max)
+        if np.isscalar(query_ra):
+            return len(inds) > 0
+        return [len(entry) > 0 for entry in inds]
+
+    def range_search(self, query_ra, query_dec, radius=None, t_min=None, t_max=None):
+        """Return the indices of the opsim pointings that fall within the field
+        of view of the query point(s).
+
+        Parameters
+        ----------
+        query_ra : float or numpy.ndarray
+            The query right ascension (in degrees).
+        query_dec : float or numpy.ndarray
+            The query declination (in degrees).
+        radius : float or None, optional
+            The angular radius of the observation (in degrees). If None
+            uses the default radius for the OpSim.
+        t_min : float, numpy.ndarray or None, optional
+            The minimum time (in MJD) for the observations to consider.
+            If None, no time filtering is applied.
+        t_max : float, numpy.ndarray or None, optional
+            The maximum time (in MJD) for the observations to consider.
+            If None, no time filtering is applied.
+
+        Returns
+        -------
+        inds : list[int] or list[numpy.ndarray]
+            Depending on the input, this is either a list of indices for a single query point
+            or a list of arrays (of indices) for an array of query points.
+        """
+        if query_ra is None or query_dec is None:
+            raise ValueError("Query RA and dec must be provided for range search, but got None.")
+
+        # Fallback to the preset radius if None is provided.
+        radius = radius if radius is not None else self.survey_values.get("radius", None)
+        if radius is None:
+            raise ValueError("Radius must be provided for range search or as a default. Got None.")
+
+        # If the points are scalars, make them into length 1 arrays.
+        is_scalar = np.isscalar(query_ra) and np.isscalar(query_dec)
+        query_ra = np.atleast_1d(query_ra)
+        query_dec = np.atleast_1d(query_dec)
+
+        # Confirm the query RA and Dec have the same length.
+        if len(query_ra) != len(query_dec):
+            raise ValueError("Query RA and Dec must have the same length.")
+        if np.any(query_ra == None) or np.any(query_dec == None):  # noqa: E711
+            raise ValueError("Query RA and dec cannot contain None.")
+
+        # Transform the query point(s) to 3-d Cartesian coordinate(s).
+        ra_rad = np.radians(query_ra)
+        dec_rad = np.radians(query_dec)
+        x = np.cos(dec_rad) * np.cos(ra_rad)
+        y = np.cos(dec_rad) * np.sin(ra_rad)
+        z = np.sin(dec_rad)
+        cart_query = np.array([x, y, z]).T
+
+        # Adjust the angular radius to a cartesian search radius and perform the search.
+        adjusted_radius = 2.0 * np.sin(0.5 * np.radians(radius))
+        inds = self._kd_tree.query_ball_point(cart_query, adjusted_radius)
+
+        if t_min is not None or t_max is not None:
+            num_queries = len(query_ra)
+            times = self.table["time"].to_numpy()
+
+            if t_min is None:
+                t_min = np.full(num_queries, -np.inf)
+            else:
+                t_min = np.atleast_1d(t_min)
+            if len(t_min) != num_queries:
+                raise ValueError(f"t_min must be a scalar or an array of length {num_queries}.")
+
+            if t_max is None:
+                t_max = np.full(num_queries, np.inf)
+            else:
+                t_max = np.atleast_1d(t_max)
+            if len(t_max) != num_queries:
+                raise ValueError(f"t_max must be a scalar or an array of length {num_queries}.")
+
+            # Run through each list of indices and filter by time. We need to do this
+            # iteratively, because the lists can have different lengths.
+            for idx, subinds in enumerate(inds):
+                if len(subinds) == 0:
+                    continue
+                time_mask = (times[subinds] >= t_min[idx]) & (times[subinds] <= t_max[idx])
+                inds[idx] = np.asarray(subinds)[time_mask]
+
+        # If the query was a scalar, we return a single list of indices.
+        if is_scalar:
+            inds = inds[0]
+        return inds
+
+    def get_observations(self, query_ra, query_dec, radius=None, t_min=None, t_max=None, cols=None):
+        """Return the observation information when the query point falls within
+        the field of view of a pointing in the survey.
+
+        Parameters
+        ----------
+        query_ra : float
+            The query right ascension (in degrees).
+        query_dec : float
+            The query declination (in degrees).
+        radius : float or None, optional
+            The angular radius of the observation (in degrees). If None
+            uses the default radius for the OpSim.
+        t_min : float or None, optional
+            The minimum time (in MJD) for the observations to consider.
+            If None, no time filtering is applied.
+        t_max : float or None, optional
+            The maximum time (in MJD) for the observations to consider.
+            If None, no time filtering is applied.
+        cols : list
+            A list of the names of columns to extract. If None returns all the
+            columns.
+
+        Returns
+        -------
+        results : dict
+            A dictionary mapping the given column name to a numpy array of values.
+        """
+        neighbors = self.range_search(query_ra, query_dec, radius, t_min=t_min, t_max=t_max)
+
+        results = {}
+        if cols is None:
+            cols = self.table.columns.to_list()
+        for col in cols:
+            # Allow the user to specify either the original or mapped column names.
+            if col not in self.table.columns:
+                raise KeyError(f"Unrecognized column name {col}")
+            results[col] = self.table[col][neighbors].to_numpy()
+        return results
+
+    def bandflux_error_point_source(self, bandflux, index):
+        """Compute observational bandflux error for a point source.
+
+        Parameters
+        ----------
+        bandflux : array_like of float
+            Band bandflux of the point source in nJy.
+        index : array_like of int
+            The index of the observation in the SurveyData table.
+
+        Returns
+        -------
+        flux_err : array_like of float
+            Simulated bandflux noise in nJy.
+        """
+        raise NotImplementedError
