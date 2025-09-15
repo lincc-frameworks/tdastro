c49f4f5cf6f15c1ec7f9b37a74279d8b7d78bb55
mi-dai <mdai23443@gmail.com>
2025-09-15 14:19:15 -0400

    Update src/tdastro/obstable/roman_obstable.py
    Co-authored-by: Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>

--- src/tdastro/obstable/roman_obstable.py
+++ src/tdastro/obstable/roman_obstable.py
@@ -89,7 +89,7 @@ class RomanObsTable(ObsTable):
     Attributes
     ----------
     table : pandas.core.frame.DataFrame
-        The table with all the OpSim information.
+        The table with all the observation information.
     colmap : dict
         A mapping of short column names to their names in the underlying table.
     _kd_tree : scipy.spatial.KDTree or None
