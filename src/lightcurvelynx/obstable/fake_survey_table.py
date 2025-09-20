6ca28554207b84f9011c375561df3b3ff53a150b
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-09-20 16:29:03 -0400

    Create an ObsTable for fake surveys with reasonable defaults

---
+++ src/lightcurvelynx/obstable/fake_survey_table.py
@@ -0,0 +1,141 @@
+import numpy as np
+
+from lightcurvelynx.astro_utils.noise_model import poisson_bandflux_std
+from lightcurvelynx.consts import GAUSS_EFF_AREA2FWHM_SQ
+from lightcurvelynx.obstable.obs_table import ObsTable
+
+
+class FakeSurveyTable(ObsTable):
+    """A subclass for a (simplified) fake survey. The user must provide the following values
+    either in the table or as keyword arguments to the constructor:
+    - fwhm : The full-width at half-maximum of the PSF in pixels.
+    - sky_background : The sky background in the units of electrons / pixel^2.
+    - zp_per_band : A dictionary mapping filter names to their instrumental zero points in nJy
+
+    Parameters
+    ----------
+    table : dict or pandas.core.frame.DataFrame
+        The table with all the ObsTable information.  Must have columns
+        "time", "ra", "dec", and "filter".
+    colmap : dict, optional
+        A mapping of standard column names to their names in the input table.
+    zp_per_band : dict
+        A dictionary mapping filter names to their instrumental zero points in nJy.
+        The filters provided must match those in the table. This is required if the
+        table does not have a zero point column.
+    **kwargs : dict
+        Additional keyword arguments to pass to the ObsTable constructor. This includes overrides
+        for survey parameters such as:
+        - dark_current : The dark current for the camera in electrons per second per pixel (default=0.0).
+        - exptime: The exposure time for the camera in seconds (default=30).
+        - fwhm: The full-width at half-maximum of the PSF in pixels (default=None).
+        - nexposure: The number of exposures per observation (default=1).
+        - radius: The angular radius of the observations in degrees (default=None).
+        - read_noise: The read noise for the camera in electrons (default=0.0).
+        - sky_background: The sky background in the units of electrons / pixel^2 (default=None).
+        - survey_name: The name of the survey (default="FAKE_SURVEY").
+    """
+
+    # Default survey values.
+    _default_survey_values = {
+        "dark_current": 0,
+        "exptime": 30,  # seconds
+        "fwhm": None,  # pixels
+        "nexposure": 1,  # exposures
+        "radius": None,  # degrees
+        "read_noise": 0,
+        "sky_background": None,
+        "survey_name": "FAKE_SURVEY",
+    }
+
+    def __init__(self, table, *, colmap=None, zp_per_band=None, **kwargs):
+        self.zp_per_band = zp_per_band
+        super().__init__(table, colmap=colmap, **kwargs)
+
+        # If the table does not contain the required columns (fwhm, sky_background,
+        # exptime, nexposure), create a constant column from the survey values.
+        self._assign_constant_if_needed("exptime", check_positive=True)
+        self._assign_constant_if_needed("nexposure", check_positive=True)
+        self._assign_constant_if_needed("fwhm", check_positive=True)
+        self._assign_constant_if_needed("sky_background", check_positive=False)
+
+    def _assign_constant_if_needed(self, colname, check_positive=True):
+        """Assign a constant column to the table if it does not already have one.
+
+        Parameters
+        ----------
+        colname : str
+            The name of the column to assign.
+        check_positive : bool, optional
+            If True, check that the value is positive before assigning it. Default is True.
+        """
+        if colname in self._table.columns:
+            return  # Already have a column.
+        if self.survey_values.get(colname) is not None:
+            value = self.survey_values[colname]
+            if check_positive and value <= 0:
+                raise ValueError(f"`{colname}` must be positive.")
+            self.add_column(colname, np.full(len(self._table), value))
+        else:
+            raise ValueError(f"Must provide valid `{colname}` for FakeSurveyTable.")
+
+    def _assign_zero_points(self):
+        """Assign instrumental zero points in nJy to the ObsTable. In this fake
+        survey, we use a constant zero point per band.
+        """
+        # Check that we either have previously assigned zero points or have been given
+        # a dictionary of zero points per band.
+        if "zp" in self._table.columns:
+            return  # Already have a column of zero points.
+        if self.zp_per_band is None:
+            raise ValueError("Must provide `zp_per_band` to FakeSurveyTable without a column of zero points.")
+        if "filter" not in self._table.columns:
+            raise ValueError(
+                "Must provide a `filter` column to FakeSurveyTable without a column of zero points."
+            )
+
+        # Check that the keys of the dictionary match the filters in the table.
+        if set(self.zp_per_band.keys()) != set(self._table["filter"].unique()):
+            raise ValueError(
+                "The keys of `zp_per_band` must match the filters in the table. "
+                f"Got zp_per_band={self.zp_per_band.keys()} and table={self._table['filter'].unique()}."
+            )
+
+        # Create a column of zero points, setting all values based on the filter column.
+        zp_col = np.zeros(len(self._table), dtype=float)
+        for key, val in self.zp_per_band.items():
+            if val <= 0:
+                raise ValueError(f"Zero point for band {key} must be positive. Got {val}.")
+            zp_col[self._table["filter"] == key] = val
+        self.add_column("zp", zp_col, overwrite=True)
+
+    def bandflux_error_point_source(self, bandflux, index):
+        """Compute observational bandflux error for a point source
+
+        Parameters
+        ----------
+        bandflux : array_like of float
+            Band bandflux of the point source in nJy.
+        index : array_like of int
+            The index of the observation in the OpSim table.
+
+        Returns
+        -------
+        flux_err : array_like of float
+            Simulated bandflux noise in nJy.
+        """
+        observations = self._table.iloc[index]
+
+        # We insert most the needed columns during construction, so we
+        # can look up most of the values needed for the noise model.
+        footprint = GAUSS_EFF_AREA2FWHM_SQ * (observations["fwhm"]) ** 2
+        return poisson_bandflux_std(
+            bandflux,
+            total_exposure_time=observations["exptime"],
+            exposure_count=observations["nexposure"],
+            footprint=footprint,
+            sky=observations["sky_background"],
+            zp=observations["zp"],
+            readout_noise=self.safe_get_survey_value("read_noise"),
+            dark_current=self.safe_get_survey_value("dark_current"),
+        )
---
+++ tests/lightcurvelynx/obstable/test_fake_survey_table.py
@@ -0,0 +1,153 @@
+import numpy as np
+import pandas as pd
+import pytest
+from lightcurvelynx.obstable.fake_survey_table import FakeSurveyTable
+
+
+def test_create_fake_survey_table_defaults():
+    """Create a minimal FakeSurveyTable object with given defaults."""
+    values = {
+        "time": np.array([0.0, 1.0, 2.0, 3.0, 4.0]),
+        "ra": np.array([15.0, 30.0, 15.0, 0.0, 60.0]),
+        "dec": np.array([-10.0, -5.0, 0.0, 5.0, 10.0]),
+        "filter": np.array(["r", "g", "r", "i", "g"]),
+    }
+    pdf = pd.DataFrame(values)
+
+    zp_per_band = {"g": 26.0, "r": 27.0, "i": 28.0}
+    ops_data = FakeSurveyTable(pdf, zp_per_band=zp_per_band, fwhm=2.0, sky_background=100.0)
+    assert len(ops_data) == 5
+
+    # We use the defaults when we do not provide values in the table.
+    assert np.allclose(ops_data["ra"], values["ra"])
+    assert np.allclose(ops_data["dec"], values["dec"])
+    assert np.allclose(ops_data["time"], values["time"])
+    assert np.array_equal(ops_data["filter"], values["filter"])
+    assert np.allclose(ops_data["fwhm"], [2.0] * 5)
+    assert np.allclose(ops_data["sky_background"], [100.0] * 5)
+    assert np.allclose(ops_data["exptime"], [30.0] * 5)
+    assert np.allclose(ops_data["nexposure"], [1] * 5)
+    assert np.allclose(ops_data["zp"], [27.0, 26.0, 27.0, 28.0, 26.0])
+
+    assert ops_data.survey_values["dark_current"] == 0
+    assert ops_data.survey_values["nexposure"] == 1
+    assert ops_data.survey_values["sky_background"] == 100
+    assert ops_data.survey_values["radius"] is None
+    assert ops_data.survey_values["read_noise"] == 0
+    assert ops_data.survey_values["survey_name"] == "FAKE_SURVEY"
+
+    # We can override the defaults.
+    ops_data = FakeSurveyTable(
+        pdf,
+        zp_per_band=zp_per_band,
+        exptime=60.0,
+        fwhm=2.5,
+        nexposure=2,
+        radius=1.0,
+        read_noise=5.0,
+        sky_background=150.0,
+        survey_name="MY_SURVEY",
+    )
+    assert len(ops_data) == 5
+
+    # We use the defaults when we do not provide values in the table.
+    assert np.allclose(ops_data["ra"], values["ra"])
+    assert np.allclose(ops_data["dec"], values["dec"])
+    assert np.allclose(ops_data["time"], values["time"])
+    assert np.array_equal(ops_data["filter"], values["filter"])
+    assert np.allclose(ops_data["fwhm"], [2.5] * 5)
+    assert np.allclose(ops_data["sky_background"], [150.0] * 5)
+    assert np.allclose(ops_data["exptime"], [60.0] * 5)
+    assert np.allclose(ops_data["nexposure"], [2] * 5)
+    assert np.allclose(ops_data["zp"], [27.0, 26.0, 27.0, 28.0, 26.0])
+
+    assert ops_data.survey_values["dark_current"] == 0
+    assert ops_data.survey_values["nexposure"] == 2
+    assert ops_data.survey_values["sky_background"] == 150
+    assert ops_data.survey_values["radius"] == 1.0
+    assert ops_data.survey_values["read_noise"] == 5.0
+    assert ops_data.survey_values["survey_name"] == "MY_SURVEY"
+
+
+def test_create_fake_survey_table():
+    """Test that if we specify values in columns, we use those instead of the defaults."""
+    values = {
+        "time": np.array([0.0, 1.0, 2.0, 3.0, 4.0]),
+        "ra": np.array([15.0, 30.0, 15.0, 0.0, 60.0]),
+        "dec": np.array([-10.0, -5.0, 0.0, 5.0, 10.0]),
+        "filter": np.array(["r", "g", "r", "i", "g"]),
+        "fwhm": np.array([2.0, 2.5, 3.0, 2.0, 1.5]),
+        "sky_background": np.array([100.0, 150.0, 200.0, 100.0, 50.0]),
+        "exptime": np.array([20.0, 30.0, 40.0, 20.0, 10.0]),
+        "nexposure": np.array([1, 2, 3, 1, 1]),
+        "zp": np.array([10.0, 11.0, 12.0, 13.0, 14.0]),
+    }
+    pdf = pd.DataFrame(values)
+
+    zp_per_band = {"g": 26.0, "r": 27.0, "i": 28.0}
+    ops_data = FakeSurveyTable(pdf, zp_per_band=zp_per_band, fwhm=2.0, sky_background=100.0)
+    assert len(ops_data) == 5
+    assert np.allclose(ops_data["ra"], values["ra"])
+    assert np.allclose(ops_data["dec"], values["dec"])
+    assert np.allclose(ops_data["time"], values["time"])
+    assert np.array_equal(ops_data["filter"], values["filter"])
+    assert np.allclose(ops_data["fwhm"], values["fwhm"])
+    assert np.allclose(ops_data["sky_background"], values["sky_background"])
+    assert np.allclose(ops_data["exptime"], values["exptime"])
+    assert np.allclose(ops_data["nexposure"], values["nexposure"])
+    assert np.allclose(ops_data["zp"], values["zp"])
+
+
+def test_create_fake_survey_table_cols_fail():
+    """Test that we raise errors when we do not provide required values."""
+    values = {
+        "time": np.array([0.0, 1.0, 2.0, 3.0, 4.0]),
+        "ra": np.array([15.0, 30.0, 15.0, 0.0, 60.0]),
+        "dec": np.array([-10.0, -5.0, 0.0, 5.0, 10.0]),
+        "filter": np.array(["r", "g", "r", "i", "g"]),
+    }
+    pdf = pd.DataFrame(values)
+
+    # Missing fwhm.
+    zp_per_band = {"g": 26.0, "r": 27.0, "i": 28.0}
+    with pytest.raises(ValueError):
+        _ = FakeSurveyTable(pdf, zp_per_band=zp_per_band, sky_background=100.0)
+
+    # Missing sky_background.
+    with pytest.raises(ValueError):
+        _ = FakeSurveyTable(pdf, zp_per_band=zp_per_band, fwhm=2.0)
+
+    # Missing or invalid exptime.
+    with pytest.raises(ValueError):
+        _ = FakeSurveyTable(pdf, zp_per_band=zp_per_band, fwhm=2.0, sky_background=100.0, exptime=None)
+    with pytest.raises(ValueError):
+        _ = FakeSurveyTable(pdf, zp_per_band=zp_per_band, fwhm=2.0, sky_background=100.0, exptime=-10.0)
+
+    # Missing or invalid nexposure.
+    with pytest.raises(ValueError):
+        _ = FakeSurveyTable(pdf, zp_per_band=zp_per_band, fwhm=2.0, sky_background=100.0, nexposure=None)
+    with pytest.raises(ValueError):
+        _ = FakeSurveyTable(pdf, zp_per_band=zp_per_band, fwhm=2.0, sky_background=100.0, nexposure=-1)
+
+
+def test_create_fake_survey_table_zp_fail():
+    """Test that we raise errors when we do not provide required values."""
+    values = {
+        "time": np.array([0.0, 1.0, 2.0, 3.0, 4.0]),
+        "ra": np.array([15.0, 30.0, 15.0, 0.0, 60.0]),
+        "dec": np.array([-10.0, -5.0, 0.0, 5.0, 10.0]),
+    }
+    pdf = pd.DataFrame(values)
+
+    # No filters from which to computer zp.
+    zp_per_band = {"g": 26.0, "r": 27.0, "i": 28.0}
+    with pytest.raises(ValueError):
+        _ = FakeSurveyTable(pdf, zp_per_band=zp_per_band, fwhm=2.0, sky_background=100.0)
+
+    # Mismatched filters.
+    values["filter"] = np.array(["r", "g", "r", "i", "z"])
+    pdf = pd.DataFrame(values)
+
+    zp_per_band = {"g": 26.0, "r": 27.0}
+    with pytest.raises(ValueError):
+        _ = FakeSurveyTable(pdf, zp_per_band=zp_per_band, fwhm=2.0, sky_background=100.0)
